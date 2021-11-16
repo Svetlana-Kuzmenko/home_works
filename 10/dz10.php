4a04573dc14f223092c521e5406af90daf19212d
Svetlana-Kuzmenko <sveto4ka357@gmail.com>
2021-11-16 22:38:09 +0200

    first commit

---
+++ 10/dz10.php
@@ -0,0 +1,33 @@
+<?php
+
+$userstxt = file_get_contents('dz7.txt');
+
+$users =explode("\n", $userstxt);
+
+$firstname=$_POST['first_name'];
+$lastname=$_POST['last_name'];
+$age=$_POST['age'];
+$gender=$_POST['gender'];
+
+$users[]=$firstname.';'. $lastname.';'.$age.';'.$gender;
+
+file_put_contents('dz7.txt', implode("\n",$users));
+
+echo 'ok!';
+
+foreach ($users as $v) {
+    $user = explode(';', trim($v));
+    $users[]=$user;}
+
+echo '<table border="1">';
+
+foreach ($users as $v){
+    echo '<tr>';
+    echo '<td>'.$v[0].'</td>';
+    echo '<td>'.$v[1].'</td>';
+    echo '<td>'.$v[2].'</td>';
+    echo '<td>'.$v[3].'</td>';
+    echo '</tr>';
+}
+echo '</table>';
+//var_dump($_GET, $_POST);
