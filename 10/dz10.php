70f32a56df1342f02c71e454e5c86363f2da25c2
Svetlana-Kuzmenko <sveto4ka357@gmail.com>
2021-11-21 19:59:38 +0200

    дз10 с выводом таблицы

--- 10/dz10.php
+++ 10/dz10.php
@@ -1,7 +1,6 @@
 <?php
 
 $userstxt = file_get_contents('dz7.txt');
-
 $users =explode("\n", $userstxt);
 
 $firstname=$_POST['first_name'];
@@ -10,18 +9,18 @@ $age=$_POST['age'];
 $gender=$_POST['gender'];
 
 $users[]=$firstname.';'. $lastname.';'.$age.';'.$gender;
-
 file_put_contents('dz7.txt', implode("\n",$users));
+//echo 'ok!';
+//var_dump ($users);
 
-echo 'ok!';
 
 foreach ($users as $v) {
     $user = explode(';', trim($v));
-    $users[]=$user;}
+    $usersarray[]=$user;}
 
 echo '<table border="1">';
 
-foreach ($users as $v){
+foreach ($usersarray as $v){
     echo '<tr>';
     echo '<td>'.$v[0].'</td>';
     echo '<td>'.$v[1].'</td>';
